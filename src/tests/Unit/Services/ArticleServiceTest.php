f3d506e1bd90e7f8ba582437f6e62437550546ad
179Bell <shingo.msdf@gmail.com>
2023-01-18 23:46:11 +0900

    createArticleテスト実装

--- src/tests/Unit/Services/ArticleServiceTest.php
+++ src/tests/Unit/Services/ArticleServiceTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Unit\Service;
 
+use App\Http\Repositories\ArticleRepository;
 use App\Http\Services\ArticleService;
 use App\Models\Article;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -11,9 +13,13 @@ class ArticleServiceTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected $articleService;
+
     public function setUp(): void
     {
         parent::setUp();
+
+        $this->articleService = $this->app->make('\App\Http\Services\ArticleService');
     }
 
     /**
@@ -28,6 +34,28 @@ class ArticleServiceTest extends TestCase
         // ステータスコードとどの画面が表示されているかの検証
         $response->assertStatus(200)
             ->assertViewIs('article.index');
+    }
+
+    /**
+     * @test
+     */
+    public function 記事を投稿してDBに値が存在する()
+    {
+        $user = User::factory()->create();
+
+        $request = [
+            'user_id' => $user->id,
+            'title' => 'test title',
+            'content' => 'test content',
+        ];
+
+        $result = $this->actingAs($user)->articleService->createArticle($request);
+
+        $this->assertDatabaseHas('articles', [
+            'user_id' => $user->id,
+            'title' => 'test title',
+            'content' => 'test content',
+        ]);
     }
 }
 
