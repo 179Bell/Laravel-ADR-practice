793c0f3edafb6f4e38a66396584d21da80822254
179Bell <shingo.msdf@gmail.com>
2023-01-21 12:49:53 +0900

    詳細ページのレイアウトを作成

--- src/resources/views/article/detail.blade.php
+++ src/resources/views/article/detail.blade.php
@@ -3,9 +3,12 @@
 @section('title', '詳細')
 
 @section('content')
-    <h2 class="d-flex justify-content-center">詳細ページ</h2>
-
-    @php
-        ddd($article);
-    @endphp
+    <h3 class="d-flex justify-content-center">詳細ページ</h3>
+    <h2>{{ $article->title  }}</h2>
+    <p>{{ $article->content }}</p>
+    <p>{{ $article->created_at }}</p>
+    @if (Auth::id() === $article->user_id)
+        <button class="btn btn-danger" onclick="location.href='#">削除する</button>
+        <button class="btn btn-success" onclick="location.href='#">編集する</button>
+    @endif
 @endsection
